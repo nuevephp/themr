aa85aa9a9eddd0f59ebc5c011ec6e6d2a1471695
Andrew <easylancer@gmail.com>
2009-06-29 14:52:32 +0100

    Updated documentation to include xml definitaion.

--- views/documentation.php
+++ views/documentation.php
@@ -7,6 +7,15 @@
 </p>
 <h2 class="subtitle"><?php echo __('Themr XML Definition file'); ?></h2>
 <p><?php echo __('In this file we define our theme information, which would be the theme name to display in themr, author, website, description and version. Check the Themr theme folder for theme.xml example'); ?></p>
+<p class="code bottom-5"><?php echo __('&lt;?xml version="1.0" encoding="UTF-8"?&gt;<br />
+&lt;themr&gt;<br />
+&nbsp;&nbsp;&lt;name&gt;Theme Name&lt;/name&gt;<br />
+&nbsp;&nbsp;&lt;author&gt;Theme Author&lt;/author&gt;<br />
+&nbsp;&nbsp;&lt;author_website&gt;http://www.themeauthorwebsite.com/&lt;/author_website&gt;<br />
+&nbsp;&nbsp;&lt;website&gt;http://www.themewebsite.com/&lt;/website&gt;<br />
+&nbsp;&nbsp;&lt;description&gt;Theme description&lt;/description&gt;<br />
+&nbsp;&nbsp;&lt;version&gt;0.1.1&lt;/version&gt;<br />
+&lt;/themr&gt;'); ?></p>
 <h2 class="subtitle"><?php echo __('Themr Convention'); ?></h2>
 <p><?php echo __('Themr uses a precise naming convention to allow the plugin to work correctly and to have less conflicts when you have more themes installed. Layouts and Snippets should always have the theme directory name before the actual filename. Snippets uses hyphen in the name and themes use underscore.'); ?></p>
 <p class="code bottom-5">
